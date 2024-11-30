c1620123bc0bdbafe6151dc9523c7e9f29904f63
Farhaan <farhaan654@gmail.com>
2024-11-30 12:17:11 -0600

    Added getters and setters

--- src/main/java/org/example/assignment4/Endpoint.java
+++ src/main/java/org/example/assignment4/Endpoint.java
@@ -2,11 +2,16 @@ package org.example.assignment4;
 
 public class Endpoint {
 
-    double x, y;
+    private double x, y;
 
     public Endpoint(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public double getX() { return this.x; }
+    public double getY() { return this.y; }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
 
 }
