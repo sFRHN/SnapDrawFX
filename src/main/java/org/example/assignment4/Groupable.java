c2406546a70b40f9a4c6ec0bd6a01fdf0d3845b9
Farhaan <farhaan654@gmail.com>
2024-12-03 19:36:11 -0600

    Updated Groupable with appropriate required methods

--- src/main/java/org/example/assignment4/Groupable.java
+++ src/main/java/org/example/assignment4/Groupable.java
@@ -1,8 +1,21 @@
 package org.example.assignment4;
 
+import javafx.scene.canvas.GraphicsContext;
+
 import java.util.List;
 
 public interface Groupable {
     boolean isGroup();
     List<Groupable> getChildren();
+    boolean contains(double mx, double my);
+    void draw(GraphicsContext gc, boolean selected);
+    void move(double dx, double dy);
+    double getLeft();
+    double getRight();
+    double getTop();
+    double getBottom();
+    void rotate(String direction);
+    void rotate(String direction, double centerX, double centerY);
+    void scale(String scale);
+    void scale(String scale, double centerX, double centerY);
 }
