6969177c6c27791f6f084e33614e1336fcd26b2d
Farhaan <farhaan654@gmail.com>
2024-12-09 06:44:31 -0600

    Implemented MoveCommand - Handles group dragging - Allows undo redo functionality

---
+++ src/main/java/org/example/assignment4/DCommands/MoveCommand.java
@@ -0,0 +1,34 @@
+package org.example.assignment4.DCommands;
+
+import org.example.assignment4.DCommand;
+import org.example.assignment4.DLine;
+import org.example.assignment4.Groupable;
+import org.example.assignment4.LineModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveCommand implements DCommand {
+
+    private LineModel model;
+    private List<Groupable> item;
+    private double startX, startY, endX, endY;
+
+    public MoveCommand(LineModel model, List<Groupable> item, double startX, double startY, double endX, double endY) {
+        this.model = model;
+        this.item = new ArrayList<Groupable>(item);
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+
+    public void doIt() {
+        model.moveItem(item, startX - endX, startY - endY);
+        model.moveItem(item, endX - startX , endY - startY);
+    }
+
+    public void undo() {
+        model.moveItem(item, startX - endX, startY - endY);
+    }
+}
